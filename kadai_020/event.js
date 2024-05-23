2301ed7bfa788560ae2b2cd3b367cc7652a80b62
K-1022 <kikunagaryu2004@gmail.com>
2024-05-23 12:16:07 +0900

    20章課題

---
+++ kadai_020/event.js
@@ -0,0 +1,14 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById("btn");
+// textというidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById("text");
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener("click", () => {
+  // li要素を新しく作成する
+  const childList = document.createElement("li");
+  // 作成したli要素に「ボタンをクリックしました」というテキストを追加する
+  childList.textContent = "ボタンをクリックしました";
+  // クリックされたら文章を置き換える
+  text.textContent = "ボタンをクリックしました";
+});
